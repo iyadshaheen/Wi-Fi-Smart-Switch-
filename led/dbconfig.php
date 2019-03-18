b91bc92868411d08410477b9db7975cc44b08860
iyadashaheen <iyadashaheen@gmail.comgit config --global user.name iyadashaheengit config --global user.email iyadashaheen@gmail.com>
2019-03-18 18:27:57 +0200

    initial commit

---
+++ led/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************/
+/************** Created by : Iyad Shaheen ************/
+/***************     And by: Abed Alaziz Shaheen *******/
+/***************     Ora-Tech     *************/
+/****************************************************/ 
+
+define('DB_USER', "id8340825_shaheen");     // Your database user name
+define('DB_PASSWORD', "gaza2884914");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id8340825_shaheen"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
